63e73bb37d014c8367ee70bd5854cb74fa192170
Davi Gupta <davigupta@gmail.com>
2020-09-11 16:26:44 -0700

    add test case for stats.py

--- minion/files/src/stats.py
+++ minion/files/src/stats.py
@@ -11,9 +11,10 @@ import asyncio
 import re
 
 OPENED = False
+MYW = 0
 
 async def pak_drop(pak, reason, log):
-    global OPENED
+    global OPENED, MYW
 
     log.error("packet drop: {}".format(reason))
     top, bottom = pak.split(b'\r\n\r\n', 1)
@@ -21,25 +22,29 @@ async def pak_drop(pak, reason, log):
     pak = re.sub(b"content-length: .*", b"content-length: 0", pak, re.MULTILINE)
     if not OPENED:
         try:
-            myr, myw = await asyncio.open_connection("null", 10000)
+            _, MYW = await asyncio.open_connection("127.0.0.1", 10000)
             OPENED = True
-            myw.write(pak)
-            try:
-                await myw.drain()
-            except (ConnectionResetError, ConnectionAbortedError) as e:
-                myw.close()
-                OPENED = False
         except :
-            pass
+            return
+    MYW.write(pak)
+    try:
+        await MYW.drain()
+    except (ConnectionResetError, ConnectionAbortedError) as e:
+        MYW.close()
+        OPENED = False
+
+async def run_test(count, log):
+    pak = b"GET / HTTP/1.1\r\nHost: gateway.sjc.nextensio.net\r\nuser-agent: shorty\r\nx-nextensio-for: 127.0.0.1\r\nx-nextensio-uuid: 123\r\ncontent-length: 23\r\n\r\n<body>\r\nhello\r\n<\body>"
+    for i in range(0, count):
+        await pak_drop(pak, "access-denied", log)
 
 if  __name__ == "__main__":
     import sys
     import logging
     logger = logging.getLogger('stats')
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    pak = b"GET / HTTP/1.1\r\nHost: gateway.sjc.nextensio.net\r\nuser-agent: shorty\r\nx-nextensio-for: 127.0.0.1\r\nx-nextensio-uuid: 123\r\ncontent-length: 23\r\n\r\n<body>\r\nhello\r\n<\body>"
     try:
-        task = pak_drop(pak, "access-denied", logger)
+        task = run_test(2, logger)
         asyncio.ensure_future(task)
         asyncio.get_event_loop().run_forever()
     finally:
