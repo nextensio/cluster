5af30f53222d3a6fa8e1bf3b04299e2fc38a43d4
Davi Gupta <davigupta@gmail.com>
2020-09-20 17:09:43 -0700

    add support for logging to file

--- nxts.io/minion.io/minion.go
+++ nxts.io/minion.io/minion.go
@@ -15,7 +15,10 @@
 package main
 
 import (
+	"fmt"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"minion.io/aaa"
 	"minion.io/args"
 	"minion.io/common"
@@ -23,9 +26,21 @@ import (
 	"minion.io/router"
 )
 
+var lumlog = &lumberjack.Logger{
+	Filename:   "app_debug.log",
+	MaxSize:    5,
+	MaxBackups: 3,
+	MaxAge:     3,
+}
+
+func lumberjackZapHook(e zapcore.Entry) error {
+	lumlog.Write([]byte(fmt.Sprintf("%+v\n", e)))
+	return nil
+}
+
 func main() {
-	//logger, _ := zap.NewProduction()
-	logger, _ := zap.NewDevelopment()
+	//logger, _ := zap.NewProduction(zap.Hooks(lumberjackZapHook))
+	logger, _ := zap.NewDevelopment(zap.Hooks(lumberjackZapHook))
 	defer logger.Sync()
 	sugar := logger.Sugar()
 	args.ArgHandler(sugar)
