07d208121f2c5ede2230536233615065b5ab9141
Davi Gupta <davigupta@gmail.com>
2020-09-22 21:40:23 -0700

    add support of mockver

--- nxts.io/minion.io/aaa/aaa.go
+++ nxts.io/minion.io/aaa/aaa.go
@@ -6,35 +6,61 @@ package aaa
 
 import (
 	"go.uber.org/zap"
-	"time"
 )
+
+var mocklib bool = true
 
 func AaaInit(ns string, uri string, s *zap.SugaredLogger) int {
 	s.Debugf("aaa: initialised in namespace %v\n", ns)
 	s.Debugf("aaa: mongo uri %v\n", uri)
+	if mocklib {
+		return 0
+	} else {
+		// put opa call
+	}
 	return 0
 }
 
 func UsrAllowed(id string, s *zap.SugaredLogger) bool {
 	s.Debugf("aaa: user allowed %v\n", id)
+	if mocklib {
+		return true
+	} else {
+		// put opa call
+	}
 	return true
 }
 
 func UsrJoin(pod string, id string, s *zap.SugaredLogger) {
 	s.Debugf("aaa: user joined %v of type %v\n", id, pod)
+	if mocklib {
+		return
+	} else {
+		// put opa call
+	}
 }
 
 func UsrLeave(pod string, id string, s *zap.SugaredLogger) {
 	s.Debugf("aaa: user left %v of type %v\n", id, pod)
+	if mocklib {
+		return
+	} else {
+		// put opa call
+	}
 }
 
 func GetUsrAttr(pod string, id string, s *zap.SugaredLogger) (string, bool) {
 	s.Debugf("aaa: usr attr %v of type %v\n", id, pod)
-	if pod == "controller" {
-		return "", false
+	if mocklib {
+		if pod == "controller" {
+			return "", false
+		}
+		usrAttr := "{ dept: computer-science, team: blue }"
+		return usrAttr, true
+	} else {
+		// put opa call
 	}
-	usrAttr := "{ dept: computer-science, team: blue }"
-	return usrAttr, true
+	return "", false
 }
 
 func AccessOk(pod string, id string, attr string, s *zap.SugaredLogger) bool {
@@ -42,15 +68,19 @@ func AccessOk(pod string, id string, attr string, s *zap.SugaredLogger) bool {
 	if pod == "agent" {
 		return true
 	}
+	if mocklib {
+		return true
+	} else {
+		// put opa call
+	}
 
 	return true
 }
 
 func AaaStart(ns string, uri string, s *zap.SugaredLogger) {
 	AaaInit(ns, uri, s)
-	s.Debug("aaa: running background task")
-	for {
-		s.Debugf("aaa: %v\n", time.Now().Format(time.RFC3339))
-		time.Sleep(60 * time.Second)
+	if mocklib {
+		return
 	}
+	// put opa call
 }
