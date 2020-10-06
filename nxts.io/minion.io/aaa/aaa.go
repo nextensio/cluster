427476424df40c1c119335439e60f171fcc6cccb
Davi Gupta <davigupta@gmail.com>
2020-10-06 15:21:44 -0700

    call authz api's

--- nxts.io/minion.io/aaa/aaa.go
+++ nxts.io/minion.io/aaa/aaa.go
@@ -6,46 +6,43 @@ package aaa
 
 import (
 	"go.uber.org/zap"
+	"minion.io/authz"
 )
 
 var mocklib bool = true
-var initDone bool = false
 var myns string
 var myuri string
 
-func AaaInit(ns string, uri string, pod string, s *zap.SugaredLogger) int {
+func AaaInit(ns string, uri string, s *zap.SugaredLogger) int {
 	s.Debugf("aaa: mongo uri %v\n", uri)
-	s.Debugf("aaa: initialised in namespace %v of type %v\n", ns, pod)
+	s.Debugf("aaa: initialised in namespace %v\n", ns)
 	if mocklib {
 		return 0
-	} else {
-		// put opa call
 	}
-	return 0
+	return (authz.NxtAaaInit(ns, uri, s))
 }
 
 func UsrAllowed(id string, s *zap.SugaredLogger) bool {
-	s.Debugf("aaa: user allowed %v\n", id)
+	var val bool
+
 	if mocklib {
-		return true
+		val = true
 	} else {
-		// put opa call
+		val = authz.NxtUsrAllowed(id)
 	}
-	return true
+
+	s.Debugf("aaa: user allowed %v : result %v\n", id, val)
+
+	return val
 }
 
 func UsrJoin(pod string, id string, s *zap.SugaredLogger) {
 	s.Debugf("aaa: user joined %v of type %v\n", id, pod)
-	if initDone == false {
-		AaaInit(myns, myuri, pod, s)
-		initDone = true
-	}
 	if mocklib {
 		return
-	} else {
-		if pod == "agent" {
-			// put opa call
-		}
+	}
+	if pod == "agent" {
+		authz.NxtUsrJoin(id)
 	}
 }
 
@@ -53,47 +50,50 @@ func UsrLeave(pod string, id string, s *zap.SugaredLogger) {
 	s.Debugf("aaa: user left %v of type %v\n", id, pod)
 	if mocklib {
 		return
-	} else {
-		if pod == "agent" {
-			// put opa call
-		}
+	}
+	if pod == "agent" {
+		authz.NxtUsrLeave(id)
 	}
 }
 
 func GetUsrAttr(pod string, id string, s *zap.SugaredLogger) (string, bool) {
-	s.Debugf("aaa: usr attr %v of type %v\n", id, pod)
+	var usrAttr string
+	var val bool
+
 	if mocklib {
 		if pod == "controller" {
 			return "", false
 		}
-		usrAttr := "{ dept: computer-science, team: blue }"
-		return usrAttr, true
+		usrAttr = "{ dept: computer-science, team: blue }"
 	} else {
-		// put opa call
+		usrAttr, val = authz.NxtGetUsrAttr(id)
 	}
-	return "", false
+	s.Debugf("aaa: usr attr %v of type %v : attr %v, val %v\n",
+		id, pod, usrAttr, val)
+	return usrAttr, val
 }
 
 func AccessOk(pod string, id string, attr string, s *zap.SugaredLogger) bool {
-	s.Debugf("aaa: access %v of type %v with attr %v\n", id, pod, attr)
+	var val bool
+
 	if pod == "agent" {
 		return true
 	}
 	if mocklib {
-		return true
+		val = true
 	} else {
-		// put opa call
+		val = authz.NxtAccessOk(id, attr)
 	}
 
-	return true
+	s.Debugf("aaa: access %v of type %v with attr %v : resullt %v\n",
+		id, pod, attr, val)
+
+	return val
 }
 
 func AaaStart(ns string, uri string, s *zap.SugaredLogger) {
 	myns = ns
 	myuri = uri
 
-	if mocklib {
-		return
-	}
-	// put opa call
+	AaaInit(myns, myuri, s)
 }
